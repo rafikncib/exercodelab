9b8090e81b2fb7437fa2be7eef3d5880894d8299
rafikncib <rafikncib@gmail.com>
2024-11-09 13:16:46 +0100

    print a new Formatted Message

---
+++ python/formattedMessage.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 13:14:50 2024
+
+@author: Rafik Ncib
+"""
+
+lang = "Python"
+version = "3.8"
+ 
+print("I am learning {} version {}".format(lang,version))
+
