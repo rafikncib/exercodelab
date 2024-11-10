d0839b30ec450df536fd1a19d2c8944dbcae9222
rafikncib <rafikncib@gmail.com>
2024-11-10 12:22:18 +0100

    add more solution

---
+++ python/sumFunction.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 13:48:11 2024
+
+@author: Rafik Ncib
+"""
+
+
+
+"""Sum between two numbers"""
+def Sum(a,b):
+    return a+b
+
+print(10,'+',25,'=',Sum(10,25))
