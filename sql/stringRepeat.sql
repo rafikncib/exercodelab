1674a0164bf92f5a54b4342c1452a467f286a305
rafikncib <rafikncib@gmail.com>
2024-11-16 23:43:41 +0100

    add solutions in sql

---
+++ sql/stringRepeat.sql
@@ -0,0 +1,8 @@
+--# write your SQL statement here: you are given a table 'repeatstr' with columns 'n' and 's', return a table with all columns and your result in a column named 'res'.
+
+SELECT 
+    s, 
+    n, 
+    repeat(s, n) AS res
+FROM 
+    repeatstr;
