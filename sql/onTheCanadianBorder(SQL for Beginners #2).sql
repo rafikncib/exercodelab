7d8772f66d68dc1c3ae79748cef4f9fa87cab6a4
rafikncib <rafikncib@gmail.com>
2024-11-18 23:04:42 +0100

    add solutions in sql

---
+++ sql/onTheCanadianBorder(SQL for Beginners #2).sql
@@ -0,0 +1,4 @@
+--Your Code Here
+select name , country
+from travelers
+where country != 'Canada' and country != 'Mexico' and country != 'USA'
---
+++ sql/oppositeNumber.sql
@@ -0,0 +1,4 @@
+-- You will be given a table: "opposite", with a column: "number". Return a table with a column: "res".
+
+select (-number) as res
+from opposite
