5eaa807587bca240a6b4dce0bec9b484c8a862e9
rafikncib <rafikncib@gmail.com>
2024-11-22 23:09:20 +0100

    add a solution in js

---
+++ javascript/descendingOrder.js
@@ -0,0 +1,14 @@
+function descendingOrder(n){
+  //c convert n to string
+  str = n.toString();
+  
+  // create an array of string characters 
+  // sort ASC then reverse them
+  array = str.split('').sort().reverse();
+  
+  // create a string from the array
+  str = array.join('');
+  
+  // finaly convert str to integer
+  return Number.parseInt(str);
+}
