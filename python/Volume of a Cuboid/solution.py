467d1265a51cc48a75782b800d9d7a28d361e46f
rafikncib <rafikncib@gmail.com>
2025-01-05 23:15:37 +0100

    add a new file solution

---
+++ python/Volume of a Cuboid/solution.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 21:58:10 2025
+
+@author: Rafik Ncib
+"""
+
+def get_volume_of_cuboid(length, width, height):
+    # Code goes here...
+    return length * width * height
