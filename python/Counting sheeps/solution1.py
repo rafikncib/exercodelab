0ae8ca66cf96b02c1f7ca4338d52005306543daa
rafikncib <rafikncib@gmail.com>
2025-01-06 18:16:40 +0100

    add a new file solution

---
+++ python/Counting sheeps/solution1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 17:16:38 2025
+
+@author: Rafik Ncib
+"""
+
+def count_sheeps(sheep):
+  return sheep.count(True)
---
+++ python/Counting sheeps/solution2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 17:20:02 2025
+
+@author: Rafik Ncib
+"""
+
+def count_sheeps(sheep):
+  # TODO May the force be with you
+  return (len([x  for x in sheep if x]))
