2af46a2d18758a7c45cda92f83f0577efd0b036c
rafikncib <rafikncib@gmail.com>
2025-01-07 23:43:10 +0100

    add a new file solution

---
+++ python/string_permutations/solution.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 10:38:45 2025
+
+@author: Rafik Ncib
+"""
+
+string = "abc"
+result = []
+long = len(string)
+
+result = ['']
+for char in string:
+    new_result = []
+    for seq in result:
+        for k in range(len(seq)+1):
+            new_char = seq[:k]+char+seq[k:]
+            new_result.append(new_char)
+    result=new_result   
+    
+print(result)
